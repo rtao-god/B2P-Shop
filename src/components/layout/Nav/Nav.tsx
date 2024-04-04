9242ee4d8be922e488b9a1864f5286549792debb
Rtao <prostodimka519@gmail.com>
2024-04-04 23:18:45 +0300

    Update Nav

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -1,39 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import './Nav.sass'
+// components/Nav.tsx
+import React from 'react';
+import './Nav.sass';
+import useApi from '@hooks/useApi'; // Adjust this path as necessary
 
 interface Brand {
-    title: string;
+  title?: string;
 }
 
-const Nav: React.FC<Brand> = () => {
-    const [brands, setBrands] = useState<Brand[]>([]);
+const Nav: React.FC = () => {
+  // Specify only the endpoint, as the base URL is included within the useApi hook
+  const { data: brands, isLoading, error } = useApi<Brand[]>('/directory/stores');
 
-    useEffect(() => {
-        const fetchBrands = async () => {
-            try {
-                const response = await fetch('https://b2pshop.click/api/v2/directory/stores');
-                const data: Brand[] = await response.json();
-                setBrands(data);
-            } catch (error) {
-                console.error('Error fetching brands:', error);
-            }
-        };
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching brands: {error.message}</div>;
 
-        fetchBrands();
-    }, []);
-
-    return (
-        <div className='main_nav container'>
-            <ul>
-                <li className="all_brands"><a href="/all_brands">Все бренды</a></li>
-                {brands.map((brand, index) => (
-                    <li className="other_brands" key={index}>
-                        <a href={"/" + brand.title.toLowerCase()}>{brand.title}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className='main_nav container'>
+      <ul>
+        <li className="all_brands"><a href="/all_brands">Все бренды</a></li>
+        {brands?.map((brand, index) => (
+          <li className="other_brands" key={index}>
+            <a href={"/" + (brand.title ? brand.title.toLowerCase() : 'unknown')}>{brand.title || 'Unknown Brand'}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Nav;
