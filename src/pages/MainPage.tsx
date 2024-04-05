76b8eb4bce7bde6061f60a15b69e25170d441c84
Rtao <prostodimka519@gmail.com>
2024-04-05 17:38:12 +0300

    Update due to new components

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,14 +2,27 @@ import Notifications from "@components/layout/Notifications/Notifications";
 import FilterBar from "@components/layout/FilterBar/FilterBar";
 import Header from "@components/layout/Header/Header";
 import Nav from "@components/layout/Nav/Nav";
+import styles from './MainPage.module.sass'
+import MainSlider from "@/components/layout/MainSlider/MainSlider";
+import Bayer from "@/components/layout/Bayer/Bayer";
+import Bestsellers from "@/components/layout/Bestsellers/Bestsellers";
+import InstallationInstructions from "@/components/layout/InstallationInstructions/InstallationInstructions";
 
 export default function MainPage() {
   return (
     <div>
       <Header />
-      <Nav />
-      <FilterBar />
-      <Notifications />
+      <div className={styles.container}>
+        <Nav />
+        <FilterBar />
+        <Notifications />
+      </div>
+      <MainSlider />
+      <div className={styles.container}>
+        <Bayer />
+        <Bestsellers />
+        <InstallationInstructions />
+      </div>
     </div>
   )
 }
