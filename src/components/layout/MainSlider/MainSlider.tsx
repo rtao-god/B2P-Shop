d2c9035b2ee46bbe1c37b1675b38e94c16fbfa34
Rtao <prostodimka519@gmail.com>
2024-04-07 11:09:16 +0300

    Added MainSlider

--- src/components/layout/MainSlider/MainSlider.tsx
+++ src/components/layout/MainSlider/MainSlider.tsx
@@ -1,56 +1,67 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
 import useApi from '@hooks/useApi';
+import styles from './MainSlider.module.sass';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Chevron from '@/components/common/icons/Chevron';
 
-interface MainSlider {
-    id?: number;
-    imageUrl?: string;
-    title?: string;
-    description?: string;
-};
+interface Slide {
+    id: string;
+    path: string;
+}
 
-const MainSlider: React.FC<MainSlider> = () => {
-    // const { data: slides, isLoading, error } = useApi<MainSlider[]>('/main-page/slides');
-    const [currentSlideIndex, setCurrentSlideIndex] = useState(0);    
+interface CustomArrowProps {
+    className?: string;
+    style?: React.CSSProperties;
+    onClick?: () => void;
+    direction: 'left' | 'right';
+}
 
-   /*  const handleNextSlide = () => {
-        setCurrentSlideIndex((prevIndex) =>
-            prevIndex === slides?.length - 1 ? 0 : prevIndex + 1
-        );
-    }; */
-
-   /*  const handlePreviousSlide = () => {
-        setCurrentSlideIndex((prevIndex) =>
-            prevIndex === 0 ? (slides?.length ?? 1) - 1 : prevIndex - 1
-        );
-    }; */
-
-    // if (isLoading) return <div>Loading...</div>;
-    // if (error) return <div>Error fetching slides: {error.message}</div>;
-    // if (!slides) return <div>No slides found.</div>;
-
-    // console.log(slides)
+const CustomArrow: React.FC<CustomArrowProps> = ({ className, style, onClick, direction }) => {
+    const arrowClass = `${styles.slickArrow} ${direction === 'left' ? styles.slickPrev : styles.slickNext}`;
 
     return (
-        <div /* style={{ width: '100vw', height: '100vh', position: 'relative' }} */>
-            {/* {slides?.map((slide, index) => ( */}
-                <div
-                    // key={slide.id}
-                    // style={{
-                    //     width: '100%',
-                    //     height: '100%',
-                    //     backgroundImage: `url(${slide.imageUrl})`,
-                    //     backgroundSize: 'cover',
-                    //     backgroundPosition: 'center',
-                    //     display: index === currentSlideIndex ? 'block' : 'none',
-                    // }}
-                >
-                    {/* <h2>{slide.title}</h2>
-                    <p>{slide.description}</p> */}
-                    l
-                </div>
-            {/* ))} */}
-            {/* <button onClick={handlePreviousSlide} style={{ position: 'absolute', left: 20, top: '50%' }}>Previous</button>
-            <button onClick={handleNextSlide} style={{ position: 'absolute', right: 20, top: '50%' }}>Next</button> */}
+        <div className={`${className || ''} ${arrowClass}`} style={style} onClick={onClick}>
+            <Chevron />
+        </div>
+    );
+};
+
+const MainSlider: React.FC = () => {
+    const { data: slides, isLoading, error } = useApi<Slide[]>('/main-page/slides');
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error fetching slides: {error.message}</div>;
+    if (!slides || slides.length === 0) return <div>No slides found.</div>;
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1.5,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        nextArrow: <CustomArrow direction="right" />,
+        prevArrow: <CustomArrow direction="left" />,
+    };
+
+    return (
+        <div className={`${styles.mainSlider} ${styles.slickDotsScoped}`}>
+            <Slider {...settings}>
+                {slides.map((slide) => (
+                    <div key={slide.id} style={{ width: '100%', position: 'relative' }}>
+                        <div className={styles.slide}
+                            style={{
+                                backgroundImage: `url(${slide.path})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                height: '258px',
+                            }}
+                        ></div>
+                    </div>
+                ))}
+            </Slider>
         </div>
     );
 };
