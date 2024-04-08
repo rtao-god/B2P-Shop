992bb49a95a46de564b9d507848f6ba02e89ce6e
Rtao <prostodimka519@gmail.com>
2024-04-08 20:22:14 +0300

    Updated header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,16 +3,14 @@ import styles from './Header.module.sass';
 
 const Header = () => {
     return (
-        <header className={styles.site_header}>
-            <div className={styles.top_bar}>
-                <div className={`${styles.container} ${styles.top_bar_content}`}>
-                    <div className={styles.logo_container}>
-                        <Logo />
-                    </div>
-                    <div className={styles.auth_buttons}>
-                        <button className={styles.btn_trans}>Вход</button>
-                        <button className={styles.btn_blue}>Регистрация</button>
-                    </div>
+        <header className={styles.header}>
+            <div className={styles.header_content}>
+                <div className={styles.logo_container}>
+                    <Logo />
+                </div>
+                <div className={styles.auth_buttons}>
+                    <button className={styles.btn_trans}>Вход</button>
+                    <button className={styles.btn_blue}>Регистрация</button>
                 </div>
             </div>
         </header>
