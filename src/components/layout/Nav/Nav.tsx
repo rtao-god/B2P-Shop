e56495c077ef9c2ace0f4195d9d9fab91e6e0a45
Rtao <prostodimka519@gmail.com>
2024-04-08 20:23:32 +0300

    I made a brand choice

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Nav.module.sass';
 import useApi from '@hooks/useApi';
 
-interface Brand {
-  title?: string;
+interface BrandProps {
+  id: string;
+  title: string;
+  available: boolean;
 }
 
 const Nav: React.FC = () => {
-  const { data: brands, isLoading, error } = useApi<Brand[]>('/directory/stores');
+  const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [selectAll, setSelectAll] = useState<boolean>(false);
+  const { data: brands, isLoading, error } = useApi<BrandProps[]>('/directory/stores');
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error fetching brands: {error.message}</div>;
+  if (error) return <div>Error fetching manual: {error.message}</div>;
+  if (!brands) return <div>No manual found.</div>;
+
+  const handleSelectAll = () => {
+    if (!selectAll) {
+      setSelectedIds(brands.filter(brand => brand.available).map(brand => brand.id));
+    } else {
+      setSelectedIds([]);
+    }
+    setSelectAll(!selectAll);
+  };
+
+  const handleBrandClick = (id: string) => {
+    const newSelectedIds = selectedIds.includes(id)
+      ? selectedIds.filter(selectedId => selectedId !== id)
+      : [...selectedIds, id];
+
+    setSelectedIds(newSelectedIds);
+
+    setSelectAll(newSelectedIds.length === brands.filter(brand => brand.available).length);
+  };
 
   return (
-    <div className={`${styles.main_nav} ${styles.container}`}>
-      <ul>
-        <li className={styles.all_brands}><a href="/all_brands">Все бренды</a></li>
-        {brands?.map((brand, index) => (
-          <li className={styles.other_brands} key={index}>
-            <a href={"/" + (brand.title ? brand.title.toLowerCase() : 'unknown')}>{brand.title || 'Unknown Brand'}</a>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.main_nav}>
+      <button className={`${styles.all_brands} ${selectAll ? styles.brand_selected : ''}`} onClick={handleSelectAll}>Все бренды</button>
+      {brands.map(brand => (
+        <button
+          key={brand.id}
+          className={`${styles.brand_button} ${!brand.available ? styles.brand_unavailable : ''} ${selectedIds.includes(brand.id) ? styles.brand_selected : ''}`}
+          onClick={() => handleBrandClick(brand.id)}
+          disabled={!brand.available}
+        >
+          {brand.title}
+        </button>
+      ))}
     </div>
   );
 };
 
-export default Nav;
+export default Nav
