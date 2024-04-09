3b84c6d1fdfe54604b159b00b8f8a0b279a97e13
Rtao <prostodimka519@gmail.com>
2024-04-09 19:04:21 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import styles from './App.module.sass';
-import './css/global/index.sass'
-import Desktop from './desktop/Desktop';
-import Mobile from './mobile/Mobile';
+import React, { useState, useEffect, Suspense } from 'react';
+import './css/global/index.sass';
+
+const Desktop = React.lazy(() => import('./desktop/Desktop'));
+const Mobile = React.lazy(() => import('./mobile/Mobile'));
 
 const App: React.FC = () => {
-  const isMobile = window.innerWidth < 720;
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 720);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 720);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <div className={styles.App}>
-      {isMobile ? <Mobile /> : <Desktop />}
+    <div className="App">
+      <Suspense fallback={<div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>Загрузка...</div>}>
+        {isMobile ? <Mobile /> : <Desktop />}
+      </Suspense>
     </div>
   );
-}
+};
 
 export default App;
